080444d3b68ddf63f582ed6928c6c8efa8641651
5ilky <ethancai920@gmail.com>
2024-09-03 22:09:33 +1000

    format file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 pub mod ast;
 pub mod loottable;
-use std::{collections::{HashMap, VecDeque}, env::args, fs::{self, read_to_string}};
+use std::{
+    collections::{HashMap, VecDeque},
+    env::args,
+    fs,
+};
 
 use ast::Expr;
 use grammar::StmtParser;
@@ -16,7 +20,7 @@ struct Context {
 
 enum RGenError {
     NoRefError(String),
-    JoinNotString()
+    JoinNotString(),
 }
 
 fn eval(ctx: &mut Context, expr: Expr) -> Result<Expr, RGenError> {
@@ -40,17 +44,17 @@ fn eval(ctx: &mut Context, expr: Expr) -> Result<Expr, RGenError> {
             println!("{}", eval(ctx, *value)?);
         }
         Expr::Weighted { value, .. } => return eval(ctx, *value),
-        Expr::Null => {},
+        Expr::Null => {}
         Expr::Join { left, right } => {
             let left_str = eval(ctx, *left)?;
             let right_str = eval(ctx, *right)?;
-            
+
             if let Expr::String(l) = left_str {
                 if let Expr::String(r) = right_str {
                     return Ok(Expr::String(l + &r));
                 }
             }
-            return Err(RGenError::JoinNotString())
+            return Err(RGenError::JoinNotString());
         }
     }
     Ok(Expr::Null)
@@ -71,9 +75,7 @@ fn random(ctx: &mut Context, expr: Expr) -> Result<Expr, RGenError> {
                 }
             }
             return if lt.total() > 0 {
-                Ok(
-                    eval(ctx, lt.random().clone())?
-                )
+                Ok(eval(ctx, lt.random().clone())?)
             } else {
                 Ok(Expr::Null)
             };
@@ -86,38 +88,34 @@ fn random(ctx: &mut Context, expr: Expr) -> Result<Expr, RGenError> {
         Expr::Display(..) => Ok(Expr::Null),
         Expr::Weighted { value, .. } => Ok(*value),
         Expr::Null => Ok(Expr::Null),
-        Expr::Join { .. } => Ok(Expr::Null)
+        Expr::Join { .. } => Ok(Expr::Null),
     }
 }
 
 fn run_line(ctx: &mut Context, src: &str, defs: &mut Vec<String>) {
-    
-        let parser = StmtParser::new();
-        let result = parser.parse(src);
-        match result {
-            Ok(v) => {
-                match v {
-                    Expr::Define { .. } => {
-                        defs.push(src.to_string())
-                    },
-                    _ => {}
-                }
-                if let Err(e) = eval(ctx, v.clone()) {
-                    match e {
-                        RGenError::NoRefError(var_name) => {
-                            eprintln!("No ref called {var_name}!");
-                        }
-                        RGenError::JoinNotString() => {
-                            eprintln!("Join operator didnt get strings as both sides");
-                        }
-
+    let parser = StmtParser::new();
+    let result = parser.parse(src);
+    match result {
+        Ok(v) => {
+            match v {
+                Expr::Define { .. } => defs.push(src.to_string()),
+                _ => {}
+            }
+            if let Err(e) = eval(ctx, v.clone()) {
+                match e {
+                    RGenError::NoRefError(var_name) => {
+                        eprintln!("No ref called {var_name}!");
                     }
-                };
-            }
-            Err(e) => {
-                eprintln!("Error: {}", e.to_string());
-            }
+                    RGenError::JoinNotString() => {
+                        eprintln!("Join operator didnt get strings as both sides");
+                    }
+                }
+            };
         }
+        Err(e) => {
+            eprintln!("Error: {}", e.to_string());
+        }
+    }
 }
 fn main() {
     let mut argv: VecDeque<String> = args().collect();
@@ -142,7 +140,7 @@ fn main() {
         let src = src.trim();
         if src == "dump" {
             fs::write("dump.rgen", defs.join("\n")).expect("Failed to dump to dump.rgen");
-            continue 'run;    
+            continue 'run;
         }
         if src == "exit" {
             break 'run;
